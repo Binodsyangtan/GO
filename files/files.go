f1b6b03d2b7d7252d84f78391d239309d64bcb63
Binodsyangtan <binodsyangtan9761@gmail.com>
2025-10-06 19:23:14 +0545

    read file

--- files/files.go
+++ files/files.go
@@ -1,29 +1,56 @@
 package main
 
 import (
+	
 	"fmt"
 	"os"
 )
 
 func main() {
 
-	file, err := os.Open("example.txt")
-	if err != nil {
-		//log the error
-		panic(err)
+	// file, err := os.Open("example.txt")
+	// if err != nil {
+	// 	//log the error
+	// 	panic(err)
 
-	}
-	fileInfo, err := file.Stat()
+	// }
+	// fileInfo, err := file.Stat()
+	// if err != nil{
+	// 	//log the error
+	// 	panic(err)
+	// }
+
+	// fmt.Println("file name:", fileInfo.Name())
+	// fmt.Println("file name:", fileInfo.IsDir())
+	// fmt.Println("file name:", fileInfo.Size())
+	// fmt.Println("file name:", fileInfo.Mode())
+	// fmt.Println("files modified at:", fileInfo.ModTime())
+
+
+	//Read file
+	// file, err := os.Open("example.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer file.Close()
+
+	// buffer := make([]byte, 40)
+	
+	// d,err := file.Read(buffer)
+	// if err != nil{
+	// 	panic(err)
+	// }
+
+	// for i := 0; i < len(buffer); i++{
+	// 	println("data",d, string(buffer[i]))
+	// }
+
+	//another way not a good way but
+	data,err := os.ReadFile("example.txt")
 	if err != nil{
-		//log the error
 		panic(err)
 	}
-
-	fmt.Println("file name:", fileInfo.Name())
-	fmt.Println("file name:", fileInfo.IsDir())
-	fmt.Println("file name:", fileInfo.Size())
-	fmt.Println("file name:", fileInfo.Mode())
-	fmt.Println("files modified at:", fileInfo.ModTime())
+	fmt.Println(string(data))
 
 
 }
